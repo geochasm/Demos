6eb7f8e9b4740e299c5938fe9876c13f75d819ef
Chad Hammond <chad.hammond@geochasm.com>
2013-06-27 12:52:27 -0600

    Initial Commit

---
+++ leafletjson/js/map_styles.js
@@ -0,0 +1,36 @@
+﻿
+var huc_hover_style = {
+    "fillColor": "#66FF00",
+    "weight": 0,
+    "fillOpacity": 1,
+    "color": "#66FF00"
+};
+
+
+var huc_default_style = {
+    "fillColor": "#CCFFCC",
+    "weight": 1,
+    "fillOpacity": .15,
+    "color": "#003300"
+};
+
+var huc_clicked_style = {
+    "fillColor": "#FFFFFF",
+    "weight": 1,
+    "fillOpacity": 0,
+    "color": "#FFFFFF"
+};
+
+var flowlines_default_style = {
+    "weight": 2,
+    "color": "#0000FF"
+};
+
+var station_default_style = {
+    fillColor: "#3300FF",
+    radius: 6,
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 1
+};
