efe6f0054dad9635e8e7c36d7f977d83a35dac50
Chad Hammond <chad.hammond@geochasm.com>
2013-07-10 15:24:48 -0600

    Initial 3d Demo Commit

---
+++ 3d/JS/buildingdata.js
@@ -0,0 +1,22 @@
+﻿var buildingTest  = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[
+              [13.37356, 52.52064],
+              [13.37350, 52.51971],
+              [13.37664, 52.51973],
+              [13.37594, 52.52062],
+              [13.37356, 52.52064]
+            ]]
+        },
+        "properties": {
+            "wallColor": "rgb(255,0,0)",
+            "roofColor": "rgb(255,128,0)",
+            "height": 500,
+            "minHeight": 0
+        }
+    }]
+};
---
+++ 3d/JS/map_styles.js
@@ -0,0 +1,39 @@
+﻿
+
+var huc_hover_style = {
+    "fillColor": "#66FF00",
+    "weight": 0,
+    "fillOpacity": 1,
+    "color": "#66FF00"
+};
+
+
+var footprint_default_style = {
+    "fillColor": "#9ee874",
+    "weight": 1,
+    "fillOpacity": 1,
+    "color": "#9ee874"
+};
+
+var huc_clicked_style = {
+    "fillColor": "#FFFFFF",
+    "weight": 1,
+    "fillOpacity": 0,
+    "color": "#FFFFFF"
+};
+
+var flowlines_default_style = {
+    "weight": 2,
+    "color": "#0000FF"
+};
+
+var station_default_style = {
+    fillColor: "#3300FF",
+    radius: 6,
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 1
+};
+
+
